cf82603d6512a1b2f7be1387476a45bd6ea89cbd
CristianoMends <mendescristiano012@gmail.com>
2024-12-09 12:37:34 -0300

    refactor: mudança na forma de gerar a chave aleatoria, validar chave, e adicionado metodo de buscarChaves por usuarioId

--- src/main/java/com/ufc/pix/service/PixKeyService.java
+++ src/main/java/com/ufc/pix/service/PixKeyService.java
@@ -1,8 +1,15 @@
 package com.ufc.pix.service;
 
+import com.ufc.pix.enumeration.KeyType;
 import com.ufc.pix.model.PixKey;
+import com.ufc.pix.model.User;
+
+import java.util.List;
+import java.util.UUID;
 
 public interface PixKeyService {
-    PixKey generatePixKey(String userId, String type);
-    boolean validatePixKey(String key);
+    PixKey generateRandomPixKey(User user);
+    PixKey registerPixKey(User user, KeyType type, String Key);
+    PixKey validatePixKey(String key);
+    List<PixKey> findAllByUser(UUID id);
 }
