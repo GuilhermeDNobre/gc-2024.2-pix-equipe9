f44bcfe549ebd5e4fc58a84ddb425148cd605fb5
CristianoMends <mendescristiano012@gmail.com>
2024-12-09 12:32:51 -0300

    refactor: adicionado atributo data, e junção com entidade User

--- src/main/java/com/ufc/pix/model/PixKey.java
+++ src/main/java/com/ufc/pix/model/PixKey.java
@@ -1,52 +1,35 @@
 package com.ufc.pix.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.ufc.pix.dto.PixKeyView;
+import com.ufc.pix.enumeration.KeyType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Entity
+@Getter
+@Setter
 public class PixKey {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     private String key;
-    private String type;
-    private String userId;
 
-    // Getters e setters
-    public UUID getId() {
-        return id;
-    }
+    @Enumerated(EnumType.STRING)
+    private KeyType type;
 
-    public void setId(UUID id) {
-        this.id = id;
-    }
+    private LocalDate date;
 
-    public String getKey() {
-        return key;
-    }
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
-    public void setKey(String key) {
-        this.key = key;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
+    public PixKeyView toView() {
+        return new PixKeyView(this.key, this.type, this.date);
     }
 }
