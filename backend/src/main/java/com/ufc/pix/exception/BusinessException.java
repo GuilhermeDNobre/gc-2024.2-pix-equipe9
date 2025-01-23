66c982182f52c36cd5a6c4d571e327b9fac4a299
CristianoMends <mendescristiano012@gmail.com>
2025-01-23 20:16:16 -0300

    feat: add classes de exceção

--- backend/src/main/java/com/ufc/pix/exception/BusinessException.java
+++ backend/src/main/java/com/ufc/pix/exception/BusinessException.java
@@ -1,10 +1,21 @@
 package com.ufc.pix.exception;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
 public class BusinessException extends RuntimeException {
+    private HttpStatus status = HttpStatus.BAD_REQUEST;
+
     public BusinessException(String message) {
         super(message);
+    }
+    public BusinessException(String message, HttpStatus status) {
+        super(message);
+        this.status = status;
     }
     public BusinessException(String message, Object ... params) {
         super(String.format(message, params));
     }
+
 }
