14b87f193263cff8c3116d4442fb38d16c5161be
joaoev <joaoalvesxp@alu.ufc.br>
2025-01-27 01:01:32 -0300

    feat: implementando a geração de reltórios pelo id do usuário e um com período opcional

--- backend/src/main/java/com/ufc/pix/service/GenerateReportsService.java
+++ backend/src/main/java/com/ufc/pix/service/GenerateReportsService.java
@@ -3,9 +3,10 @@ package com.ufc.pix.service;
 import com.ufc.pix.dto.ViewGenerateReportsDto;
 import com.ufc.pix.dto.ViewUserDto;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface GenerateReportsService {
-    Optional<ViewGenerateReportsDto> generateByUserId(UUID userId);
+    Optional<ViewGenerateReportsDto> generateByUserId(UUID userId, LocalDateTime startDate, LocalDateTime endDate);
 }
