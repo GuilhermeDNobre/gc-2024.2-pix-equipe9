6e6ccb5f8bd7267513524a34eaf8eae1e07bbaa3
joaoev <joaoalvesxp@alu.ufc.br>
2025-01-26 22:20:58 -0300

    feat: update controller GenerateReportsController

--- backend/src/main/java/com/ufc/pix/controller/GenerateReportsController.java
+++ backend/src/main/java/com/ufc/pix/controller/GenerateReportsController.java
@@ -1,22 +1,24 @@
 package com.ufc.pix.controller;
 
+import com.ufc.pix.dto.ViewGenerateReportsDto;
+import com.ufc.pix.service.GenerateReportsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/generate-reports")
+@RequestMapping("generate-reports")
 public class GenerateReportsController {
     @Autowired
     GenerateReportsService generateReportsService;
 
-    @GetMapping("/accounts/{id}")
-    public ResponseEntity<ViewGenerateReportsDto> generateReports(@PathVariable(value = "id") UUID id) {
-        return ResponseEntity.ok(this.generateReportsService.generateByUserId(id));
+    @GetMapping("/{userId}")
+    public ResponseEntity<Optional<ViewGenerateReportsDto>> generateReports(@PathVariable(value = "userId") UUID userId) {
+        return ResponseEntity.ok(this.generateReportsService.generateByUserId(userId));
     }
 }
