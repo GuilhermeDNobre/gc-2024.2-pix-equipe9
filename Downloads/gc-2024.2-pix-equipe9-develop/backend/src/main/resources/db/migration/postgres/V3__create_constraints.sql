04ca634ecbe19d91b202b32e9b3d7ad08a2600d4
Guilherme D <gui.damascenonobre52@gmail.com>
2025-01-27 10:21:20 -0300

    refactor: Ordenação dos arquivos

--- Downloads/gc-2024.2-pix-equipe9-develop/backend/src/main/resources/db/migration/postgres/V3__create_constraints.sql
+++
@@ -1,7 +0,0 @@
--- Constraint de chave estrangeira na tabela accounts
-ALTER TABLE accounts
-ADD CONSTRAINT fk_account_user FOREIGN KEY (user_id) REFERENCES users(id);
-
--- Constraint de chave estrangeira na tabela users
-ALTER TABLE users
-ADD CONSTRAINT fk_user_account FOREIGN KEY (account_id) REFERENCES accounts(id);
