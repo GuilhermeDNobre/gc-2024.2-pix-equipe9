4f6265ad507b444881e3d34c7f0d12344ec01a9f
CristianoMends <mendescristiano012@gmail.com>
2025-01-27 12:09:54 -0300

    refactor: ajustado rotas

--- backend/src/main/java/com/ufc/pix/security/SecurityFilter.java
+++ backend/src/main/java/com/ufc/pix/security/SecurityFilter.java
@@ -1,12 +1,16 @@
 package com.ufc.pix.security;
 
-import com.ufc.pix.service.impl.TokenService;
+import com.ufc.pix.exception.BusinessException;
+import com.ufc.pix.repository.UserRepository;
+import com.ufc.pix.service.TokenService;
+import com.ufc.pix.service.impl.TokenServiceImpl;
 import com.ufc.pix.service.UserService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -18,15 +22,15 @@ import java.io.IOException;
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
     @Autowired
-    TokenService tokenService;
+    private TokenService tokenService;
     @Autowired
-    UserService userService;
+    private UserRepository userService;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = this.recoveryToken(request);
         if(token != null){
             var subject = tokenService.validateToken(token);
-            UserDetails user = userService.findByEmail(subject);
+            UserDetails user = userService.findByEmail(subject).orElseThrow(()->new BusinessException("User not found", HttpStatus.NOT_FOUND));
 
             var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
             SecurityContextHolder.getContext().setAuthentication(authentication);
