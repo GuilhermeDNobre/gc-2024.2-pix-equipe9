f71ac350e0654d17cedc047f48dc797c0c120257
joaoev <joaoalvesxp@alu.ufc.br>
2025-01-27 01:02:08 -0300

    feat: implementando o controller GenerateReportsController

--- backend/src/main/java/com/ufc/pix/controller/GenerateReportsController.java
+++ backend/src/main/java/com/ufc/pix/controller/GenerateReportsController.java
@@ -6,19 +6,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("generate-reports")
 public class GenerateReportsController {
+
     @Autowired
-    GenerateReportsService generateReportsService;
+    private GenerateReportsService generateReportsService;
 
     @GetMapping("/{userId}")
-    public ResponseEntity<Optional<ViewGenerateReportsDto>> generateReports(@PathVariable(value = "userId") UUID userId) {
-        return ResponseEntity.ok(this.generateReportsService.generateByUserId(userId));
+    public ResponseEntity<Optional<ViewGenerateReportsDto>> generateReports(
+            @PathVariable(value = "userId") UUID userId,
+            @RequestParam(value = "startDate", required = false) String startDate,
+            @RequestParam(value = "endDate", required = false) String endDate
+    ) {
+        LocalDateTime start = startDate != null ? LocalDateTime.parse(startDate) : null;
+        LocalDateTime end = endDate != null ? LocalDateTime.parse(endDate) : null;
+
+        Optional<ViewGenerateReportsDto> report = generateReportsService.generateByUserId(userId, start, end);
+        return ResponseEntity.ok(report);
     }
 }
