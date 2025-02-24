c585dbcc5f1b8946ec795b70c7e7f474174f1eae
joaoev <joaoalvesxp@alu.ufc.br>
2025-02-24 00:17:34 -0300

    feat: criando endpoint no controller para listar todas as atividades de login pelo id do usuário

--- backend/src/main/java/com/ufc/pix/controller/LoginActivityController.java
+++ backend/src/main/java/com/ufc/pix/controller/LoginActivityController.java
@@ -1,13 +1,24 @@
 package com.ufc.pix.controller;
 
+import com.ufc.pix.dto.ViewLoginActivityDto;
+import com.ufc.pix.model.Notification;
 import com.ufc.pix.service.LoginActivityService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/login-activity")
 public class LoginActivityController {
     @Autowired
     private LoginActivityService loginActivityService;
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<ViewLoginActivityDto> getAllByUserId(@PathVariable UUID userId) {
+        return  ResponseEntity.ok(loginActivityService.listAllByUserId(userId).orElseThrow(() -> new RuntimeException("No login activity found")));
+    }
 }
