6e2d00b583e1ad88a1aa038769f480ddccf206e6
joaoev <joaoalvesxp@alu.ufc.br>
2025-02-23 22:41:44 -0300

    feat: criando repository LoginActivityRepository

---
+++ backend/src/main/java/com/ufc/pix/repository/LoginActivityRepository.java
@@ -0,0 +1,10 @@
+package com.ufc.pix.repository;
+
+import com.ufc.pix.model.LoginActivity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface LoginActivityRepository extends JpaRepository<LoginActivity, UUID> {
+
+}
